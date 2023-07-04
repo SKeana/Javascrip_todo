13d569f2c89f94f5c8107e6ac6cf6511b3ead5a3
SKeana <128926074+SKeana@users.noreply.github.com>
2023-07-04 09:08:01 +0100

    Create register.js

---
+++ register.js
@@ -0,0 +1,13 @@
+document.getElementById('registerForm').addEventListener("submit", function(event) {
+    event.preventDefault ();
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    var user = {
+        username: username,
+        password: password
+    };
+    localStorage.setItem(username, JSON,stringify(user));
+    window.Location.hrft = "login.html";
+});
